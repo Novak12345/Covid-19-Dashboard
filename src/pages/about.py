2c9e091bebe29f0c979093b33b3f01aaf4c70444
Novak12345 <73523641+Novak12345@users.noreply.github.com>
2021-07-22 23:17:35 +0530

    Add files via upload

---
+++ src/pages/about.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def main():
+    st.title("About")
+    st.info(
+        """
+        This app is maintained by Arohan. You can learn more about me at
+        https://sites.google.com/view/arohan/.
+        """
+)
