4a489d24569b204f56248449c8e543dc3b43bf5f
Novak12345 <73523641+Novak12345@users.noreply.github.com>
2021-07-25 19:39:08 +0530

    Update about.py

--- src/pages/about.py
+++ src/pages/about.py
@@ -1,8 +1,10 @@
 import streamlit as st
-
+from background import set_png_as_page_bg
 
 def main():
+    set_png_as_page_bg('x.png')
     st.title("About")
+    st.balloons()
     st.info(
         """
         This app is maintained by Arohan. You can learn more about me at
